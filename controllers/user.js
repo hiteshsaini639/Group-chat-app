dd4513cbc03aff039bea9837b33a13829c0e79d7
hitesh saini <hiteshsaini639@gmail.com>
2022-10-10 18:24:54 +0530

    added login backend logic

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
 const User = require("../models/user");
@@ -37,6 +38,47 @@ exports.postUser = (req, res, next) => {
     .catch((err) => {
       if (err.type === "error") {
         res.status(403).send(err);
+      } else {
+        console.log(err);
+        res.status(500).send(err);
+      }
+    });
+};
+
+exports.loginUser = (req, res, next) => {
+  let existingUser;
+  const { email, password } = req.body;
+  if (isNotValid(email) || isNotValid(password)) {
+    return res
+      .status(400)
+      .send({ type: "error", message: "Invalid Form Data!" });
+  }
+  User.findOne({ where: { email: email } })
+    .then((user) => {
+      if (!user) {
+        throw { type: "error", message: "User Not Found!" };
+      } else {
+        existingUser = user;
+        return bcrypt.compare(password, user.password);
+      }
+    })
+    .then((result) => {
+      if (result) {
+        const token = jwt.sign(
+          {
+            userId: existingUser.id,
+            userEmail: existingUser.email,
+          },
+          process.env.TOKEN_SECRET_KEY
+        );
+        res.status(200).send(token);
+      } else {
+        res.status(401).send({ type: "error", message: "Wrong Password!" });
+      }
+    })
+    .catch((err) => {
+      if (err.type === "error") {
+        res.status(404).send(err);
       } else {
         console.log(err);
         res.status(500).send(err);
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -3,13 +3,15 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   axios
-    .post("http:localhost:3000/user/login", {
+    .post("http://localhost:3000/user/login", {
       email: e.target.email.value,
       password: e.target.password.value,
     })
     .then((response) => {
       if (response.status === 200) {
-        localStorage.setItem("sessionToken", response.data.sessionToken);
+        localStorage.setItem("sessionToken", response.data);
+        console.log(response.data);
+        // notify(response.data);
       } else {
         throw { response: response };
       }
