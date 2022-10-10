68f454210071b075ff7de34010661cb95ed03613
hitesh saini <hiteshsaini639@gmail.com>
2022-10-10 17:57:25 +0530

    creating login screen

---
+++ public/scripts/login.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  axios
+    .post("http:localhost:3000/user/login", {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        localStorage.setItem("sessionToken", response.data.sessionToken);
+      } else {
+        throw { response: response };
+      }
+    })
+    .catch((err) => {
+      notify(err.response.data);
+      console.log(err);
+    });
+});
